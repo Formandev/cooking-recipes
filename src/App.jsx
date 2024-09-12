27d7da912d27be8fe35f2dc442eaa3c60c698ce2
RedVVest <nadolskyy.fake@gmail.com>
2024-09-12 19:29:56 +0300

    created prekol

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import './App.css';
-import {useGlobalContext} from "./context"
+import "./App.css";
+import { useGlobalContext } from "./context";
 
-
-import Favorites from './components/Favorites';
-import Meals from './components/Meals';
-import Modal from './components/Modal';
-import Search from './components/Search';
+import Favorites from "./components/Favorites";
+import Meals from "./components/Meals";
+import Modal from "./components/Modal";
+import Search from "./components/Search";
+import Img from "./assets/zelinski.jpeg";
 
 export default function App() {
-  const {showModal, favorites} = useGlobalContext()
+  const { showModal, favorites } = useGlobalContext();
   return (
     <main>
-        <Search/>
-        {favorites.length > 0 && <Favorites/>}
-        
-        <Meals/>
-        {showModal && <Modal/>}
-        
+      {/* <Search />
+      {favorites.length > 0 && <Favorites />} */}
+      <div className="flex">
+        <img src={Img} alt="" />
+        <div className="flex-2">
+          <p>Вітаю Вас мобілізовано!</p>
+          <p>Сайт забираємо собі, будемо робити біштеки</p>
+        </div>
+      </div>
+
+      {/* <Meals />
+      {showModal && <Modal />} */}
     </main>
-  )
+  );
 }
