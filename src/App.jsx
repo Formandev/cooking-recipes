2120de9dd760f7b6dc726e1f0aae4595408fd0b6
RedVVest <nadolskyy.fake@gmail.com>
2024-09-13 12:59:56 +0300

    Added music

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import "./App.css";
 import { useGlobalContext } from "./context";
+import { useEffect, useRef } from "react";
 
 import Favorites from "./components/Favorites";
 import Meals from "./components/Meals";
 import Modal from "./components/Modal";
 import Search from "./components/Search";
-import Img from "./../assets/assets/zelinski-D0T-gleN.jpeg";
+import Img from "./../assets/assets/zelinski-D0T-gleN-D0T-gleN.jpeg";
+import song from "./../assets/assets/song-Bp4y7Q69.mp3";
 
 export default function App() {
   const { showModal, favorites } = useGlobalContext();
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  }, []);
+
   return (
     <main>
       {/* <Search />
@@ -20,6 +30,8 @@ export default function App() {
           <p>Сайт забираємо собі, будемо робити біштеки</p>
         </div>
       </div>
+
+      <audio ref={audioRef} src={song} />
 
       {/* <Meals />
       {showModal && <Modal />} */}
