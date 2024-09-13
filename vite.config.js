5982689b584b2bf3be38c185a8a63590000ee00a
RedVVest <nadolskyy.fake@gmail.com>
2024-09-13 11:46:21 +0300

    Fixed base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cooking-recipes/",
+  base: "https://formandev.github.io/cooking-recipes/",
   plugins: [react()],
 });
