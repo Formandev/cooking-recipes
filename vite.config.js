186d84be4dd397036216d55a923ea8103e5c5276
RedVVest <nadolskyy.fake@gmail.com>
2024-09-13 11:54:55 +0300

    Changed base output

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cooking-recipes/",
+  base: "/cooking-recipes/", // base URL for GitHub Pages
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
+  },
 });
